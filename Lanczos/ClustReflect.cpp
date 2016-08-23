7a7cf49800a344d7f22a5ecb456e5ab740a9f213
Dariusz Mikołajczuk <radomik@gmail.com>
2016-08-23 20:31:28 +0200

    Compute final eigen vectors loop

--- Lanczos/ClustReflect.cpp
+++ Lanczos/ClustReflect.cpp
@@ -1,5 +1,7 @@
 
 typedef uint32_t u32;
+typedef uint64_t u64;
+typedef double flt;
 
 ///TODO: Fully rewrite this in C++, it is only draft for now
 
@@ -16,9 +18,12 @@ for (u32 k = 1, i = 0; k < mm; k++) {
 ci.push_back(pair(i, mm));
 
 // reflect using Householder
-vector<double> e; //TODO: use doubles everywhere, not floats, use u32 instead of u32 everywhere but size_t for u32*u32
-vector<double> c;
-double* S2 = new double[m * ci.size()]; // array of m rows and ci.size() columns
+vector<flt> e(ci.size()); //TODO: use doubles everywhere, not floats, use u32 instead of u32 everywhere but size_t for u32*u32
+vector<flt> c(ci.size());
+vector<flt> x;
+vector<flt> u;
+
+flt* S2 = new flt[m * ci.size()]; // array of m rows and ci.size() columns
 for (u32 i = 0; i < ci.size(); i++) {
 	pair<u32,u32> cii = ci[i];
 	max_cul_idx = 0;               // j
@@ -31,17 +36,19 @@ for (u32 i = 0; i < ci.size(); i++) {
 	}
 	ji = cii.left + max_cul_idx;
 	// x and u vectors are of size no more than mm
-	x = {S[0][cii.left], S[0][cii.left+1], ..., S[0][cii.right]}
+	// x = {S[0][cii.left], S[0][cii.left+1], ..., S[0][cii.right]}
+	x.resize(cii.right - cii.left + 1);
+	memcpy(x.data(), S+cii.left, x.size());
 	u = x;
 	u[max_cul_idx] += sign(x[max_cul_idx]) * x.norm();
 	
 	// s = S(:,ji)-2/(u'*u)*(S(:,cii)*u)*u(j);
 	// S2(:,i) = s;
-	double un = u.normPow();
-	double uj = u[max_cul_idx];
+	flt un = u.normPow();
+	flt uj = u[max_cul_idx];
 	for (u32 p = 0; p < m; p++) { //TODO: paralellize this loop with ISPC/OpenMP
-		double st = 0.0;
-		size_t pm = p*m; 
+		flt st = 0.0;
+		u64 pm = p*m; 
 		for (u32 q = cii.left, r = 0; q <= cii.right; q++, r++) {
 			st += S[pm + q] * u[r];
 		}
@@ -52,6 +59,63 @@ for (u32 i = 0; i < ci.size(); i++) {
 		idx[q] = false; // idx is a 0/1 vector obtained from Rescon step
 	}
 	idx[ji] = true;
-	e.push_back(ritz[ji]);
-	c.push_back(lres[ji]);
+	e[i] = ritz[ji];
+	c[i] = lres[ji];
 }
+
+// e is a vector of computed eigen values
+
+// compute eigen vectors
+vector<flt> v = startvec; // startvec from initial Lanczos iteration input
+vector<flt> v2;
+flt* X = new flt[n * ci.size()];
+flt a, b, a2, b2 = 0.0;
+
+for (u32 k = 0; ; k++) {
+	flt* S2k = &S2[k * ci.size()];
+	for (u32 i = 0; i < n; i++) {
+		flt* Xi = &X[i*ci.size()];
+		for (u32 j = 0; j < ci.size(); j++) {
+			Xi[j] += v[i] * S2k[j];
+		}
+	}
+	
+	if (k == m-1) {
+		break;
+	}
+	
+	if (k == 0) {
+		TrMatrixd::mulByVector(r, A, v);  // r{n} = A{n}{n} * v{n}
+	} else {
+		TrMatrixd::mulByVector(rt, A, v);  // rt{n} = A{n}{n} * v{n}
+		Vecd::mulByScalar(vt, v2, b2);     // vt{n} = b2{1} * v2{n}
+		Vecd::sub(r, rt, vt);              // r{n}  = rt{n} - vt{n}
+	}
+	
+	a = Vecd::dotProduct(v, r);  // a{1} = SUM v[i]*r[i]
+
+	Vecd::mulByScalar(vt, v, a); // vt{n}   = a{1} * v{n}
+	Vecd::sub(r, r, vt);         // r{n}    = r{n} - vt{n}
+	b = r.getNorm();
+	v2 = v;
+	a2 = a;
+	b2 = b;
+	Vecd::divByScalar(v, r, b); // EACH_i v[i] = r[i]/b
+}
+
+vector<flt> xv(ci.size(), 0.0);
+
+// xv is a vector of sums of squared elements in each column of X
+for (u32 i = 0; i < n; i++) {
+	flt* Xi = &X[i*ci.size()];
+	for (u32 j = 0; j < ci.size(); j++) {
+		xv[j] += Xi[j]*Xi[j];
+	}
+}
+
+for (u32 i = 0; i < ci.size(); i++) {
+	// xv[i] = 1.0 / diag(X)[i] / sqrt(xv[i])
+	xv[i] = 1.0 / X[i*ci.size() + i] / sqrt(xv[i]);
+}
+
+Matrixd::mulByVector(X, X, xv);
